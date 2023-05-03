5c2cb450a034099915cfa61c49cad132999b0796
kmhbDOes <kmhbipul@gmail.com>
2023-05-03 13:47:04 +0600

    Added blog questions

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -4,7 +4,22 @@ const Blog = () => {
   return (
     <div>
       <div className="text-center">
-        <h2>This is blog</h2>
+        <p>
+          Differences between uncontrolled and controlled components
+          <span></span>
+        </p>
+        <p>
+          How to validate React props using PropTypes?
+          <span></span>
+        </p>
+        <p>
+          Tell us the difference between nodejs and express JS
+          <span></span>
+        </p>
+        <p>
+          What is a custom hook, and why will you create a custom hook?
+          <span></span>
+        </p>
       </div>
     </div>
   );
